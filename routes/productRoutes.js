0650d8f3c62b7068120d0c4d14c9ff55e0ee5f8e
29Niro <29niro@gmail.com>
2023-05-04 19:27:21 +0530

    user authorizations completed

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+
 const { getProducts, createProduct, getProduct, updateProduct, deleteProduct } = require("../controllers/productController");
 
 router.route("/").get(getProducts).post(createProduct);
